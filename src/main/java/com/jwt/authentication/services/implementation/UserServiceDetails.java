f40869a1a4d6cf10301a6827f79e2de2a95e2447
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-28 16:52:41 -0500

    Feat: Clase que se encarga de cargar un usuario con sus roles para hacer la comprobación de los permisos

---
+++ src/main/java/com/jwt/authentication/services/implementation/UserServiceDetails.java
@@ -0,0 +1,37 @@
+package com.jwt.authentication.services.implementation;
+
+import com.jwt.authentication.entities.User;
+import com.jwt.authentication.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class UserServiceDetails implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if(user == null){
+            throw new UsernameNotFoundException("Invalid username or password.");
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), getAuthority(user));
+    }
+
+    private Set<SimpleGrantedAuthority> getAuthority(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        user.getRoles().forEach(role -> {
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getName()));
+        });
+        return authorities;
+    }
+}
