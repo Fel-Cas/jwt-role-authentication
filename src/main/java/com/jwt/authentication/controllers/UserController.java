9c2ca5b61274b7b8112419499ca3d595280e26c5
Andrés Castro <andres.cmonsalve@gmail.com>
2022-09-28 16:44:02 -0500

    Feat: Creación del controller para el modelo User

---
+++ src/main/java/com/jwt/authentication/controllers/UserController.java
@@ -0,0 +1,73 @@
+package com.jwt.authentication.controllers;
+
+import com.jwt.authentication.config.JwtAuthenticationFilter;
+import com.jwt.authentication.config.TokenProvider;
+import com.jwt.authentication.dto.AuthToken;
+import com.jwt.authentication.dto.LoginUser;
+import com.jwt.authentication.dto.UserDto;
+import com.jwt.authentication.entities.Role;
+import com.jwt.authentication.entities.User;
+import com.jwt.authentication.exceptions.errors.ForbiddenException;
+import com.jwt.authentication.services.implementation.CurrentUser;
+import com.jwt.authentication.services.interfaces.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private CurrentUser currentUser;
+
+
+   @PostMapping("/register")
+    public User saveUser(@RequestBody UserDto user){
+        return userService.save(user);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping()
+    public ResponseEntity<List<User>> getAll(){
+       List<User> users=this.userService.findAll();
+       return ResponseEntity.ok(users);
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN','USER')")
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable Long id){
+       User currentuser=this.currentUser.getCurrentUser();
+       if(currentuser.getId()!=id && !isAdmin(currentuser)) throw new ForbiddenException("You can't view this user");
+       return  ResponseEntity.ok(this.userService.findById(id));
+    }
+
+    protected  boolean isAdmin(User user){
+       List<Role>roles= (List<Role>) user.getRoles().stream().filter(role->role.getName()=="ADMIN").collect(Collectors.toList());
+       return roles.size()==1;
+   }
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/hello-admin")
+    public String adminPing(){
+        return "Only Admins Can Read This";
+    }
+
+    @PreAuthorize("hasAnyRole('ADMIN', 'USER')")
+    @GetMapping("/hello-admin-user")
+    public String adminUser(){
+        return "Only Admins and Users Can Read This";
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/hello-user")
+    public String userPing(){
+        return "Any User Can Read This";
+    }
+}
